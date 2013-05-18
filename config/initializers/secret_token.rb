f8213c3bd400ef28ae23acec258acdf40873d5e7
Tim Cort <timcort@gmail.com>
2013-05-18 17:33:53 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CareerSmarter::Application.config.secret_token = '8732cbc3f518a34ad27e4f739b377332262487b07011dfa6ddbb5d44beee7509df4635b4f15fbd356a30e5923fdf6e23d940d947f3178d7810b81ae6e1a6b0e9'
